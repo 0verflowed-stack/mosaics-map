8d9012bccc9f977abbcbc976497227ba869baebd
0verflowed-stack <spaydermendima@gmail.com>
2024-11-28 00:32:20 +0200

    Add basic code for mosaics map in stalker 2

---
+++ src/components/MapView/index.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
+import L from 'leaflet';
+import mapData from './mapData.json';
+import 'leaflet/dist/leaflet.css';
+
+const MapView = () => {
+  const initialZoom = mapData.mapConfig.initial_zoom || 4;
+  const startLat = mapData.mapConfig.start_lat || 0;
+  const startLng = mapData.mapConfig.start_lng || 0;
+
+  const [activeCategories, setActiveCategories] = useState(['11899']); // Object.keys(mapData.categories)
+
+  const getCategoryIcon = (categoryId) => {
+    const category = mapData.categories[categoryId];
+    return L.icon({
+      iconUrl: `/icons/${category.icon}.png`,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+    });
+  };
+
+  const toggleCategory = (categoryId) => {
+    setActiveCategories((prev) =>
+      prev.includes(categoryId)
+        ? prev.filter((id) => id !== categoryId)
+        : [...prev, categoryId]
+    );
+  };
+
+  const tileUrl = `https://tiles.mapgenie.io/games/${mapData.mapConfig.tile_sets[0].pattern}`;
+
+  return (
+    <div>
+      {/* Category Filters */}
+      <div className="category-filters">
+        {Object.values(mapData.categories).map((category) => (
+          <label key={category.id}>
+            <input
+              type="checkbox"
+              checked={activeCategories.includes(category.id.toString())}
+              onChange={() => toggleCategory(category.id.toString())}
+            />
+            {category.title}
+          </label>
+        ))}
+      </div>
+
+      {/* Map */}
+      <MapContainer center={[startLat, startLng]} zoom={initialZoom} style={{ height: '90vh', width: '100%' }}>
+        <TileLayer
+          url={tileUrl}
+          minZoom={mapData.mapConfig.tile_sets[0].min_zoom}
+          maxZoom={mapData.mapConfig.tile_sets[0].max_zoom}
+          attribution="&copy; Your Attribution"
+        />
+
+        {/* Regions */}
+        {mapData.regions.map((region) =>
+          region.features.map((feature) => {
+            const coordinates = feature.geometry.coordinates[0].map((coord) => [coord[1], coord[0]]);
+            const style = mapData.styles.regionStyles[region.id] || {};
+
+            return (
+              <Polygon
+                key={feature.id}
+                positions={coordinates}
+                pathOptions={{
+                  color: style['line-color'] || 'blue',
+                  fillColor: style['fill-color'] || 'blue',
+                  fillOpacity: style['fill-opacity'] || 0,
+                }}
+              />
+            );
+          })
+        )}
+
+        {/* Locations */}
+        {mapData.locations
+          .filter((location) => activeCategories.includes(location.category_id.toString()))
+          .map((location) => (
+            <Marker
+              key={location.id}
+              position={[location.latitude, location.longitude]}
+              icon={getCategoryIcon(location.category_id)}
+            >
+              <Popup>
+                <h3>{location.title}</h3>
+                <p>{location.description}</p>
+                {location.media &&
+                  location.media.map((mediaItem) => (
+                    <img key={mediaItem.id} src={mediaItem.url} alt={mediaItem.title} style={{ width: '100%' }} />
+                  ))}
+              </Popup>
+            </Marker>
+          ))}
+
+        {/* Notes */}
+        {mapData.notes.map((note) => (
+          <Marker key={note.id} position={[note.latitude, note.longitude]}>
+            <Popup>
+              <h3>{note.title}</h3>
+              <p>{note.description}</p>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapView;
